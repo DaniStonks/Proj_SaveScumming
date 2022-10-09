f586d10da8a383f787a1f9590fa1cfae6f0293c1
DaniStonks <200221067@estudantes.ips.pt>
2022-10-09 11:32:53 +0100

    load da grid para 2d array em script

--- Assets/Scripts/Grid/Grid.cs
+++ Assets/Scripts/Grid/Grid.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+
+    private GameObject[,] grid; //[linha, coluna]
     // Start is called before the first frame update
     void Start()
     {
-        
+        grid = new GameObject[10,10];
+        SetUpGrid();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetUpGrid(){
+        GameObject graphicGrid = GameObject.FindGameObjectWithTag("Grid");
+        int row = 0, col = 0;
+        foreach(Transform rowLine in graphicGrid.transform){
+            foreach(Transform rowTile in rowLine){
+                grid[row, col] = rowTile.gameObject;
+                col++;
+            }
+            row++;
+            col = 0;
+        }
     }
 }
