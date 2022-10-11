395ba099d2d5822dca1ea593aa14fe100a7592a2
Rafael-NSilva <80034802+Rafael-NSilva@users.noreply.github.com>
2022-10-11 12:52:53 +0100

    ui

--- Assets/Scripts/Grid/Tile.cs
+++ Assets/Scripts/Grid/Tile.cs
@@ -42,7 +42,7 @@ public class Tile : MonoBehaviour
         {
             isSelected = true;
 
-            // TODO - Mostrar UI
+            GameObject.Find("Canvas").transform.Find("UIPanel").gameObject.SetActive(true);
         }
     }
 
